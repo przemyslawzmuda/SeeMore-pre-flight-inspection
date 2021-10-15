4d6172f8ae165c4c7eb01f1c8e5e36320bad42d0
przemyslawzmuda <88719600+przemyslawzmuda@users.noreply.github.com>
2021-10-15 22:31:13 +0200

    Put problems right

--- Model/SeeMoreSoftware/LetsMeetData.py
+++ Model/SeeMoreSoftware/LetsMeetData.py
@@ -5,12 +5,12 @@ import random
 class LetsMeetData:
     @staticmethod
     def walkThroughData(root_path):
-        '''
+        """
         :param root_path:
         The function walkThroughData() walks through the main directory called as a parameter root_path,
         in order to read subdirectories as well as files placed in the root_path. Informations according to
         the root_path will be displayed.
-        '''
+        """
         filesNumber = 0
         for directoryPath, subDirNames, fileNames in os.walk(root_path, topdown=True):
             # os.walk yields a 3-tuple: (dirpath, dirnames, filenames)
@@ -20,14 +20,14 @@ class LetsMeetData:
 
     @staticmethod
     def createDictionaryPathsAndFiles(root_path):
-        '''
+        """
         :param root_path:
         :return: dataDictionary - {path_to_folder (String): images_names (List)}
         The following function takes as in input the data type String (root_path), walks through that main directory
         and returns a dictionary data type called 'dataDictionary'. The keys are path to the subdirectories located
         in the root_path as well as this the values are list data type contains names of the images located
         inside the subdirectories.
-        '''
+        """
         dataDictionary = {}
         for directoryPath, subDirNames, fileNames in os.walk(root_path, topdown=True):
             # os.walk yields a 3-tuple: (dirpath, dirnames, filenames)
@@ -44,25 +44,25 @@ class LetsMeetData:
 
     @staticmethod
     def returnRandomImageFromDirectory(root_path):
-        '''
+        """
         :param root_path:
         :return: outrightPathToRandomImage
         The following function returns a path to the random image from the root_path directory.
-        '''
+        """
         # Get a dictionary {'path': [img1, img2, ..., img_n]}
         imagesDictionary = LetsMeetData.createDictionaryPathsAndFiles(root_path)
         # Get items from imagesDictionary as dict_items: dict_items( [ (path1, [images]) ] )
-        imagesDictionaryItems = imagesDictionary.items() # dict_items() object
+        imagesDictionaryItems = imagesDictionary.items()  # dict_items() object
         # Convert the following dict_items() object into a List data type: [ (path1, [images1], ... ]
         imagesDictionaryItemsList = list(imagesDictionaryItems)
         # Shuffle the following list, it does not return anything, only reorganize the existing list
         random.shuffle(imagesDictionaryItemsList)
         # Get a random tuple
-        randomTuple = random.choice(imagesDictionaryItemsList) # List data type: [ (path1, [images1], ... ]
+        randomTuple = random.choice(imagesDictionaryItemsList)  # List data type: [ (path1, [images1], ... ]
         # Split the the tuple components into the two variables
         pathToImages, imagesList = randomTuple
         # Return randomize the imagesList using random.sample(sequence, k)
-        randomImagesList = random.sample(imagesList, len(imagesList)) # k = len(imagesList)
+        randomImagesList = random.sample(imagesList, len(imagesList))  # k = len(imagesList)
         # Choose a random sample (image) from the randomImagesList
         randomImage = random.choice(randomImagesList)
         # create a complete path to the image
