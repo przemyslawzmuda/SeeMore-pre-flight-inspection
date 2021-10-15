23abfc514a6dbe1960603e9c410ed14e78907cf0
przemyslawzmuda <88719600+przemyslawzmuda@users.noreply.github.com>
2021-10-15 20:29:41 +0200

    Add documentation strings to the walkThroughData() and createDictionaryPathsFiles() functions.

--- Model/SeeMoreSoftware/LetsMeetData.py
+++ Model/SeeMoreSoftware/LetsMeetData.py
@@ -4,6 +4,12 @@ import os
 class LetsMeetData:
     @staticmethod
     def walkThroughData(root_path):
+        '''
+        :param root_path:
+        The function walkThroughData() walks through the main directory called as a parameter root_path,
+        in order to read subdirectories as well as files placed in the root_path. Informations according to
+        the root_path will be displayed.
+        '''
         filesNumber = 0
         for directoryPath, subDirNames, fileNames in os.walk(root_path, topdown=True):
             # os.walk yields a 3-tuple: (dirpath, dirnames, filenames)
@@ -13,6 +19,14 @@ class LetsMeetData:
 
     @staticmethod
     def createDictionaryPathsAndFiles(root_path):
+        '''
+        :param root_path:
+        :return: dataDictionary - {path_to_folder (String): images_names (List)}
+        The following function takes as in input the data type String (root_path), walks through that main directory
+        and returns a dictionary data type called 'dataDictionary'. The keys are path to the subdirectories located
+        in the root_path as well as this the values are list data type contains names of the images located
+        inside the subdirectories.
+        '''
         dataDictionary = {}
         for directoryPath, subDirNames, fileNames in os.walk(root_path, topdown=True):
             # os.walk yields a 3-tuple: (dirpath, dirnames, filenames)
