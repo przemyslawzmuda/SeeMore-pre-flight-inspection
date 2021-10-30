072df274da32692e2816299dec4589a7ed2bf7f0
przemyslawzmuda <88719600+przemyslawzmuda@users.noreply.github.com>
2021-10-30 09:48:06 +0200

    Edit callback

--- Model/Callbacks/StopTraining.py
+++ Model/Callbacks/StopTraining.py
@@ -4,9 +4,10 @@ from tensorflow.keras.callbacks import Callback
 class StopTrainingCallback(Callback):
     def __init__(self, accuracy: float):
         """
-        :param accuracy: the value of the training accuracy as a float number. If the given accuracy value is reached,
-        the training process will be terminated.
+        The training process will be terminated, if the given accuracy value is reached.
+        :param accuracy: the value of the training accuracy as a float number.
         """
+        super(Callback, self).__init__()
         self.accuracy = accuracy
 
     def on_epoch_end(self, epoch, logs={}):
