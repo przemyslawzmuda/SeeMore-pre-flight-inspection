84ce74e13955439bb47ece3d706927d804e9941b
przemyslawzmuda <88719600+przemyslawzmuda@users.noreply.github.com>
2021-10-31 23:55:31 +0100

    Update DataInput class to raise customized exception

--- IO/DataInput.py
+++ IO/DataInput.py
@@ -1,8 +1,11 @@
+from Exception.InputIntMismatchException import InputIntMismatchException
+
+
 class DataInput:
 
     @staticmethod
     def input_int(number: int):
         try:
-            return number
-        except:
-            raise
+            return int(number)
+        except ValueError:
+            raise InputIntMismatchException(number) from None
