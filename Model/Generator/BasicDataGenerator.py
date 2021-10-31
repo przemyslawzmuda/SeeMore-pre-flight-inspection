3b509847ec7adb13f02a4b8d73215b2de011c582
przemyslawzmuda <88719600+przemyslawzmuda@users.noreply.github.com>
2021-10-31 22:06:20 +0100

    BasicZoomGenerator class

--- Model/Generator/BasicDataGenerator.py
+++ Model/Generator/BasicDataGenerator.py
@@ -14,8 +14,32 @@ class BasicGenerator:
         """
         self.rescale = rescale
 
-    def createGenerator(self) -> object:
+    def create_basic_generator(self) -> object:
         """
+        The following function performs normalizing pixels data into the range between 0 and 1: (0, 1).
         :rtype: <class 'keras.preprocessing.image.ImageDataGenerator'>
         """
         return ImageDataGenerator(rescale=self.rescale)
+
+
+class BasicZoomGenerator(BasicGenerator):
+    """
+    BasicZoomGenerator is used to generate batches of tensor image WITH real-time data augmentation applying
+    zoom parameter.
+    """
+
+    def __init__(self, rescale: float, zoom_range: list):
+        """
+        :param rescale: rescaling factor, defaults to None.
+        :param zoom_range: Float or list [lower, upper]. Range for random zoom.
+        """
+        super(BasicGenerator, self).__init__(rescale)
+        self.zoom_range = zoom_range
+
+    def create_zoom_generator(self) -> object:
+        """
+        The following function performs normalizing pixels data into the range between 0 and 1 applying rescaling
+        factor, as well as this performs data augmentation applying zooming factor.
+        :rtype: <class 'keras.preprocessing.image.ImageDataGenerator'>
+        """
+        return ImageDataGenerator(rescale=self.rescale, zoom_range=self.zoom_range)
