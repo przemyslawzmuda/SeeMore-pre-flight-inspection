1d784880b1b83a1281288f1f76c2f5f53cee07ce
przemyslawzmuda <88719600+przemyslawzmuda@users.noreply.github.com>
2021-11-08 08:47:07 +0100

    Update the class to return a file path with Tkinter

--- IO/InputFilePath.py
+++ IO/InputFilePath.py
@@ -1,12 +1,25 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 
 
-class InputFilePath:
+class InputFilePathWithTkinter:
+    def __init__(self, information_message):
+        """
+        :param information_message: Display the information message with intentions to User.
+        """
+        self.information_message = information_message
 
-    @staticmethod
-    def return_file_path():
+    '''
+    Options for message boxes with Tkinter:
+    showinfo, showwarning, showerror, askquestion, askokcancel, askyesno.
+    '''
+
+    def show_info_message(self):
+        messagebox.showinfo("Information", self.information_message)
+
+    def return_file_path(self):
         root = tk.Tk()
         root.withdraw()
+        self.show_info_message()
         file_path = filedialog.askopenfilename()
         return file_path
