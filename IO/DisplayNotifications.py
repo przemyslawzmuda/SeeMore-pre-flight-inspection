a9f9076426e60170d0aac173dea5019345af58d3
przemyslawzmuda <88719600+przemyslawzmuda@users.noreply.github.com>
2021-11-08 15:02:23 +0100

    Creat box information about process

---
+++ IO/DisplayNotifications.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import messagebox
+
+
+class ShowInformationToUser:
+    def __init__(self, message_info):
+        self.message_info = message_info
+
+    def show_message(self):
+        messagebox.showinfo("Process information", self.message_info)
+
+    def display_notification(self):
+        root = tk.Tk()
+        root.withdraw()
+        self.show_message()
