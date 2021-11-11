86e250621b9c21025fabf9934715aba3ccdbe3ac
przemyslawzmuda <88719600+przemyslawzmuda@users.noreply.github.com>
2021-11-11 15:03:15 +0100

    Create simpledialog application to ask for integer number

---
+++ IO/DataInputWithTkinter.py
@@ -0,0 +1,14 @@
+from tkinter.simpledialog import askinteger
+from IO.TkinterApplication import RunTkinterApplication
+
+
+class AskUserForIntegerNumber(RunTkinterApplication):
+
+    def __init__(self, message, min_value, max_value):
+        super().__init__(message)
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def displaySomething(self) -> int:
+        int_number = askinteger("Enter number", self.message, minvalue=self.min_value, maxvalue=self.max_value)
+        return int_number
