65ccb8cbbe9bd28baf18f56c2742baba84d95300
przemyslawzmuda <88719600+przemyslawzmuda@users.noreply.github.com>
2021-11-11 15:01:59 +0100

    Create baseline for tkinter implementation

---
+++ IO/TkinterApplication.py
@@ -0,0 +1,15 @@
+import tkinter
+
+
+class RunTkinterApplication:
+    def __init__(self, message):
+        self.message = message
+
+    def displaySomething(self):
+        pass
+
+    def runApp(self):
+        window_root = tkinter.Tk()
+        window_root.withdraw()
+        self.displaySomething()
+        window_root.destroy()
