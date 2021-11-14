a8be051e5630440ce939cdf766e7cb65d723f25a
przemyslawzmuda <88719600+przemyslawzmuda@users.noreply.github.com>
2021-11-14 11:57:05 +0100

    Create a function in order to prepare and normalize a custom image to perform a prediction using trained neural network model.

--- Model/SeeMoreSoftware/DrawConlusionsFromDeepLearning/DrawConlusionsStructure.py
+++ Model/SeeMoreSoftware/DrawConlusionsFromDeepLearning/DrawConlusionsStructure.py
@@ -1,3 +1,4 @@
+import tensorflow as tfl
 from tensorflow.keras.models import load_model
 import matplotlib.pyplot as mpyplot
 from IO.IOTkinter.DataOutputWithTkinter.DisplayNotifications import DisplayErrorNotification
@@ -82,6 +83,7 @@ class ConclusionsStructure:
             path to the saved model or h5py.File object from which to load the model.
         :return: A Keras model instance.
         """
+
         try:
             loaded_model = load_model(path_to_saved_model, compile=True)
             return loaded_model
@@ -89,3 +91,63 @@ class ConclusionsStructure:
             DisplayErrorNotification("Unable to load a model from hdf5 or h5py format file.").runNotification()
         except IOError:
             DisplayErrorNotification("Unable to load a model because of an invalid savefile.").runNotification()
+
+    def normalizeImage(self, path_to_image: str, image_shape: int):
+        """
+        The following function enables to upload a custom image and prepare it in order to make a prediction
+        using convolutional neural network. The function transforms a custom image into a tensor matrix and reshapes it
+        into the following format: (image_shape, image_shape, colour_channels).
+        Attention:
+        The parameter image_shape shall be compatible with the parameter input_shape prescribed in a convolutional
+        neural network.
+        :param path_to_image: Absolute path into the custom image.
+        :param image_shape: The desired shape of the normalized image.
+        :return: tensor image.
+        """
+
+        try:
+            '''
+            This operation returns a tensor with the entire contents of the input filename. It does not do any parsing, it 
+            just returns the contents as they are. Usually, this is the first step in the input pipeline. 
+            Reads the contents of file:
+            '''
+            image = tfl.io.read_file(path_to_image)
+
+            '''
+            Function for decode_bmp, decode_gif, decode_jpeg, and decode_png. Detects whether an image is a BMP, GIF, 
+            JPEG, or PNG, and performs the appropriate operation to convert the input bytes string into 
+            a Tensor of type dtype.
+            Returns: Tensor with type dtype and a 3- or 4-dimensional shape, depending on the file type and the value 
+            of the expand_animations parameter.
+            Args: 
+            contents=image -> A Tensor of type string. 0-D. The encoded image bytes.
+            channels=3 -> An optional int. Defaults to 0. Number of color channels for the decoded image.
+            expand_animations=True -> An optional bool. Defaults to True. Controls the shape of the returned op's output. 
+            If True, the returned op will produce a 3-D tensor for PNG, JPEG, and BMP files; and a 4-D tensor for all GIFs, 
+            whether animated or not. If, False, the returned op will produce a 3-D tensor for all file types and will 
+            truncate animated GIFs to the first frame.
+            '''
+            decoded_image = tfl.io.decode_image(image, channels=3, expand_animations=True)  # True is default value
+
+            '''
+            Resize the image into the prescribed size suing the specified method.
+            
+            Args:
+            images=decoded_image -> 4-D Tensor of shape [batch, height, width, channels] or 
+                3-D Tensor of shape [height, width, channels].
+            size=[image_shape, image_shape] -> A 1-D int32 Tensor of 2 elements: new_height, new_width. 
+                The new size for the images.
+                
+            Returns:
+            If images was 4-D, a 4-D float Tensor of shape [batch, new_height, new_width, channels]. 
+            If images was 3-D, a 3-D float Tensor of shape [new_height, new_width, channels].
+            '''
+            resized_image = tfl.image.resize(decoded_image, size=[image_shape, image_shape])
+
+            # Normalize an image into the float pixels value
+            normalized_image = resized_image / 255.0
+
+            return normalized_image
+
+        except ValueError as err:
+            DisplayErrorNotification(f"Unable to normalize an image because of {err}.").runNotification()
