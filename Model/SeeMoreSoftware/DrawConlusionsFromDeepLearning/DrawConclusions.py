ec4a1f18d5b7e237306b5d50d10e82f7d7480224
przemyslawzmuda <88719600+przemyslawzmuda@users.noreply.github.com>
2021-11-14 11:09:09 +0100

    Create function to return class names from ImageDataGenerator

--- Model/SeeMoreSoftware/DrawConlusionsFromDeepLearning/DrawConclusions.py
+++ Model/SeeMoreSoftware/DrawConlusionsFromDeepLearning/DrawConclusions.py
@@ -33,3 +33,20 @@ class DrawConclusionsController:
         mpyplot.legend()
 
         mpyplot.show()
+
+    def getClassNamesFromGenerator(self, data_from_generator: object) -> list:
+        """
+        The following function returns the list contains class names from a configured TensorFlow generator.
+        The dictionary containing the mapping from class names to class indices can be obtained
+        via the attribute class_indices. It works for 'flow_from_directory' method in ImageDataGenerator object.
+        Classes can be accessed through the classes Args of 'flow_from_directory'. Optional list of class subdirectories
+        (e.g. ['dogs', 'cats']). Default: None. If not provided, the list of classes will be automatically inferred from
+        the subdirectory names/structure under directory, where each subdirectory will be treated as a different class
+        (and the order of the classes, which will map to the label indices, will be alphanumeric).
+
+        :param data_from_generator: Configured object generator, ex.: ImageDataGenerator
+        :return: class_names_list
+        """
+        # Use List comprehension
+        class_names_list = [key for key in data_from_generator.class_indices.keys()]
+        return class_names_list
