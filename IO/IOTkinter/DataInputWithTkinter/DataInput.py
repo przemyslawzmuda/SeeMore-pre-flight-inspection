3ae3ea7bca06bac3d3436f8b59b6c3ae429da294
przemyslawzmuda <88719600+przemyslawzmuda@users.noreply.github.com>
2021-11-16 14:13:09 +0100

    Add a simpledialog form

--- IO/IOTkinter/DataInputWithTkinter/DataInput.py
+++ IO/IOTkinter/DataInputWithTkinter/DataInput.py
@@ -1,4 +1,4 @@
-from tkinter.simpledialog import askinteger
+from tkinter.simpledialog import askinteger, askstring
 from IO.IOTkinter.ConfigureTkinterApplication import ConfigureTkinterNotification
 
 
@@ -10,3 +10,13 @@ class AskUserForIntegerNumber(ConfigureTkinterNotification):
     def configureNotification(self) -> int:
         int_number = askinteger("Enter number", self.message)
         return int_number
+
+
+class AskUserForString(ConfigureTkinterNotification):
+
+    def __init__(self, message):
+        super().__init__(message)
+
+    def configureNotification(self) -> str:
+        custom_string = askstring("custom_string", self.message)
+        return custom_string
