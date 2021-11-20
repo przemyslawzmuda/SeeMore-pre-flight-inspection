d42e37f97b4b6fee54838fefd90e0206d0fb1cc3
przemyslawzmuda <88719600+przemyslawzmuda@users.noreply.github.com>
2021-11-20 18:31:16 +0100

    Customize a documentation in the ModelConfiguration class.

--- Model/CnnModel/ModelConfiguration.py
+++ Model/CnnModel/ModelConfiguration.py
@@ -1,50 +1,46 @@
 class ModelConfiguration:
-    """
-    A class used to configure a neural network model in oder to start a learning process.
+    """A class used to configure a neural network model in oder to start a learning process.
 
     Class Object Attributes
-    ----------------------------
-    recognizeWingComponents : bool and recognizeAircraftPoundings : bool
+    ----------
+    recognizeWingComponents : bool and recognizeAircraftPoundings : `bool`
         A boolean value of the variable in order to indicate the region of application of the class.
-    all_history_names : list
-        A list which stores historyName variables passed during the initializing of the instance.
-    all_ModelConfiguration_instances : list
+    all_history_names : `list`
+        A list which stores history_name variables passed during the initializing of the instance.
+    all_ModelConfiguration_instances : `list`
         A list which stores all instances of the following class (stores objects).
 
     Dynamic Attributes
-    ----------------------------
-    optimizer : object or str
+    ----------
+    optimizer : `object` or `str`
         String (name of optimizer) or an instance of the optimizer. Can be used from tfl.keras.optimizers.
-    lossFunction : object or str
+    lossFunction : `object` or `str`
         String (name of a loss function) or an instance of the loss function. Can be used from tlf.keras.losses.
-    historyName : str
+    history_name : `str`
         Variable name in order to indicate and retrieve the training metrics on completion the learning process.
-    trainingGenerator : object
+    trainingGenerator : `object`
         An instance of the ImageDataGenerator class for the training data.
-    validationGenerator : object
+    validationGenerator : `object`
         An instance of the ImageDataGenerator class for the validation data.
-    epochsNumber : int
+    epochsNumber : `int`
         A number of the estimated epochs during the training process.
     **kwargs
-        ownCallback : list
-            A list which contains string values of the customized callbacks.
+        ownCallback : `list`
+        A list which contains string values of the customized callbacks.
 
     Methods
-    ----------------------------
-    compileModel(model)
-        :param model: A hierarchical structure of the neural network.
+    ----------
+    compileModel(model: object) -> None
+        :param model:
+    createHistoryAndRunModel(model: object) -> object
 
-    createHistoryAndRunModel(model)
-        :param model: A compiled hierarchical structure of the neural network.
-        :return historyName: A list which contains the metrics of the learning process (ex.: training accuracy,
-            validation accuracy, loss function values and validation function values).
     """
 
     # Class Object Attributes - Define default values
     recognizeWingComponents = True
     recognizeAircraftPoundings = False
 
-    # Use Class Object Attribute to track all historyName variables used during compiling the model
+    # Use Class Object Attribute to track all history_name variables used during compiling the model
     all_history_names = []
 
     # Use Class Object Attribute to track all the given instances of the class
@@ -52,17 +48,15 @@ class ModelConfiguration:
 
     # Using Class Object Attributes define for what type of problems the ModelConfiguration is valid to implement
     if recognizeWingComponents or recognizeAircraftPoundings:
-        def __init__(self, optimizer: object or str, lossFunction: object or str, historyName: str,
+        def __init__(self, optimizer: object or str, lossFunction: object or str, history_name: str,
                      trainingGenerator: object, epochsNumber: int, validationGenerator: object, **kwargs):
             """
             The following constructor enables to initialize the crucial parameters needed during compiling
             the neural network model.
 
-            Parameters
-            ----------------------------
             :param optimizer: String (name of optimizer) or an instance of the optimizer --> tfl.keras.optimizers.
             :param lossFunction: String (name of a loss function) or an instance of the loss function.
-            :param historyName: String (name of the training history saved during the learning).
+            :param history_name: String (name of the training history saved during the learning).
             :param trainingGenerator: An instance of the ImageDataGenerator class for the training data.
             :param epochsNumber: A number of the estimated epochs during the training process.
             :param validationGenerator:  An instance of the ImageDataGenerator class for the validation data.
@@ -73,38 +67,76 @@ class ModelConfiguration:
             self.optimizer = optimizer  # self refers to the particular object
             self.lossFunction = lossFunction
 
-            self.historyName = historyName
-            ModelConfiguration.all_history_names.append(self.historyName)
+            self.history_name = history_name
+            ModelConfiguration.all_history_names.append(self.history_name)
 
             self.trainingGenerator = trainingGenerator
             self.epochsNumber = epochsNumber
             self.validationGenerator = validationGenerator
-            # Use keyword arguments to for the __init__() method to overload a constructor
+            # Use keyword arguments in the __init__() method in order to overload the constructor
             self.callback = kwargs.get('ownCallback')
 
             # Append an instance object into the Class Object Attribute
             ModelConfiguration.all_ModelConfiguration_instances.append(self)
 
-    def compileModel(self, model):
+    def compileModel(self, model: object) -> None:
+        # Use a reStructuredText Example for documentation
+        """
+        The following function can be used to compile a hierarchical neural network model in order to
+        start the learning process.
+
+        :param model: A hierarchical structure of the neural network.
+        :type model: object
+
+        :returns: None
+        :rtype: None
+        """
+
         model.compile(
             optimizer=self.optimizer,
             loss=self.lossFunction,
             metrics=["accuracy"]
         )
 
-    def createHistoryAndRunModel(self, model) -> list:
+    def createHistoryAndRunModel(self, model: object) -> object:
+        # Use a NumPy/SciPy Docstrings Example for documentation
+        """The following method is used to start the learning process.
+
+        Parameters
+        ----------------------------
+        model : object
+            A compiled hierarchical structure of the neural network.
+
+        Returns
+        ----------------------------
+        history_name : object
+            A history object. Its History.history attribute is a record of training loss values and metrics values
+            at successive epochs, as well as validation loss values and validation metrics values (if applicable).
+
+        Raises
+        ----------------------------
+        RunTimeError
+            The following exception would be raised:
+            --> if the model was never compiled;
+            --> if the model.fit was wrapped in tfl.function.
+
+        ValueError
+            The following exception would be raised, if the mismatched between the provided input data and what
+            the model excepts occurred or when the input data was empty.
+        """
+
         if self.callback:
-            self.historyName = model.fit(
+            self.history_name = model.fit(
                 self.trainingGenerator,
                 epochs=self.epochsNumber,
                 validation_data=self.validationGenerator,
                 callbacks=self.callback
             )
-            return self.historyName
+            return self.history_name
         else:
-            self.historyName = model.fit(
+            self.history_name = model.fit(
                 self.trainingGenerator,
                 epochs=self.epochsNumber,
                 validation_data=self.validationGenerator,
             )
-            return self.historyName
+            return self.history_name
