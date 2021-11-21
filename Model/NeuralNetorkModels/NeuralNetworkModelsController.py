9a41ff392f97b799ded1c3dae17dedff627b3d24
przemyslawzmuda <88719600+przemyslawzmuda@users.noreply.github.com>
2021-11-21 16:55:39 +0100

    Create an outline in order to implement a controller to manage the learning process

---
+++ Model/NeuralNetorkModels/NeuralNetworkModelsController.py
@@ -0,0 +1,19 @@
+class ConfigureAndRunLearningProcess:
+    pass
+
+    # Configure a generator for the training data set
+
+    # Configure a generator for the validation data set
+
+    # Load a training data in from directories into the training generator, turning it into the batches
+
+    # Load a validation data in from directories into the validation generator, turning it into the batches
+
+    # Return a hierarchical structure of the neural network
+
+    # Compile the neural network model
+
+    # Fit the neural network model subsequently start the learning process
+
+
+
